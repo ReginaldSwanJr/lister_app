c01f1243007006240db824208c1c2007c46e898f
reginaldswanjr <reginaldswan@Reginalds-MBP.knology.net>
2020-06-07 11:08:33 -0400

    small changes

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Item = (props) => (
     <li>
         <ul>
+            <li>item</li>
             <li>{props.username}</li>
             <li>{props.password}</li>
         </ul>
